0233fd923031e62faefadaf8e5f2b902e6ff8960
Filip Szkarlat Szczepaniak <filip.szkarlat@gmail.com>
2025-08-19 16:11:45 +0200

    create testimonial desktop without picture parameter

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
       </TestimonialsDesktop>
 
         <TestimonialsDesktop name={'May Andersons'} company={'Workcation'} title={'CTO'}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.
       </TestimonialsDesktop>
     </>
   )
--- src/TestimonialsDesktop.jsx
+++ src/TestimonialsDesktop.jsx
@@ -1,4 +1,6 @@
 import quote from './testimonialAssets/quote.svg'
+import dotPattern from './testimonialAssets/DotPattern.svg'
+import mark from './testimonialAssets/Mark.svg'
 
 export default function TestimonialsDesktop({picture, children, name, title, company}) {
   return (
@@ -22,6 +24,25 @@ export default function TestimonialsDesktop({picture, children, name, title, com
       </div>
     : 
       <div className="testimonial no_picture">
+        <img className='dot-pattern' src={dotPattern} />
+        <div className="testimonial_content">
+          <div className='tertimonial_header'>
+            <img src={mark} alt='' className="quote" />
+            <h2>Work<span>cation</span></h2>
+          </div>
+          <div className="testimonial_description">
+            {`"${children}"`}
+          </div>
+          <div className="testimonial_footer">
+            <div className="name">
+              {name ? name : 'Name Surname'}
+            </div>
+            <span>/</span>
+            <div className="job_title">
+              {`${company ? company : 'Company Name'}, ${title ? title : 'Job Title'}`}
+            </div>
+          </div>
+        </div>
       </div>
   )
 }
