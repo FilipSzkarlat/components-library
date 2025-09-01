3d8e940019e67d47b5426056143cd357f11eb0ee
Filip Szkarlat Szczepaniak <filip.szkarlat@gmail.com>
2025-09-01 15:42:47 +0200

    create toast popup component

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import image from './components/TestimonialAssets/Image.png'
 import imageMobile from './components/TestimonialAssets/ImageMobile.png'
 import TestimonialMobile from './components/TestimonialsMobile.jsx';
 import Tooltip from './components/Tooltip.jsx';
+import ToastPopup from './components/ToastPopup.jsx';
 
 export default function App() {
 
   return (
     <>
-      {/* <Banner type="success"></Banner>
+      <Banner type="success"></Banner>
       <Banner type="success">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
       </Banner>
@@ -29,39 +30,39 @@ export default function App() {
       <Banner type="neutral"></Banner>
       <Banner type="neutral">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.
-      </Banner> */}
+      </Banner>
 
-      {/* <Card>
+      <Card>
         Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
       </Card>
 
       <Card title="Next to do">
         Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-      </Card> */}
+      </Card>
 
-      {/* <TestimonialsDesktop picture={image} name={'May Andersons'} company={'Workcation'} title={'CTO'}>
+      <TestimonialsDesktop picture={image} name={'May Andersons'} company={'Workcation'} title={'CTO'}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. 
       </TestimonialsDesktop>
 
       <TestimonialsDesktop name={'May Andersons'} company={'Workcation'} title={'CTO'}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.
-      </TestimonialsDesktop> */}
+      </TestimonialsDesktop>
 
-      {/* <TestimonialMobile picture={imageMobile} name={'May Andersons'} company={'Workcation'} title={'CTO'}>
+      <TestimonialMobile picture={imageMobile} name={'May Andersons'} company={'Workcation'} title={'CTO'}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
       </TestimonialMobile>
 
         <TestimonialMobile picture={imageMobile}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
-      </TestimonialMobile> */}
+      </TestimonialMobile>
 
-      {/* <TestimonialMobile name={'May Andersons'} company={'Workcation'} title={'CTO'}>
+      <TestimonialMobile name={'May Andersons'} company={'Workcation'} title={'CTO'}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
       </TestimonialMobile>
 
         <TestimonialMobile>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit.
-      </TestimonialMobile> */}
+      </TestimonialMobile>
 
       <Tooltip>Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.</Tooltip>
 
@@ -78,6 +79,18 @@ export default function App() {
       <Tooltip mode='light' color='pink'>Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.</Tooltip>
 
       <Tooltip mode='light' color='green'>Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.</Tooltip>
+
+
+
+      <div className='toast-popup-container'>
+        <ToastPopup />
+
+        <ToastPopup type='warning' />
+
+        <ToastPopup type='error' />
+
+        <ToastPopup type='information' />
+      </div>
     </>
   )
 }
---
+++ src/components/ToastPopup.jsx
@@ -0,0 +1,43 @@
+import success from './ToastPopupIcons/success.svg';
+import error from './ToastPopupIcons/error.svg';
+import warning from './ToastPopupIcons/warning.svg';
+import information from './ToastPopupIcons/information.svg';
+
+export default function ToastPopup({type="success"}) {
+    let title='Success';
+    let message = 'Your work has been saved.';
+    let icon = <img src={success} alt="" />;
+
+    if (type === 'error') {
+        title = 'Error';
+        message = 'Please re-save your work again.';
+        icon = <img src={error} alt />;
+    } else if (type === 'warning') {
+        title = 'Warning';
+        message = 'A network error was detected.';
+        icon = <img src={warning} alt="" />;
+    } else if (type === 'information') {
+        title = 'Information';
+        message = 'Please read updated information.';
+        icon = <img src={information} alt="" />;
+    }
+
+    setTimeout(() => {
+        const toasts = document.querySelectorAll('.toast-popup-container');
+        setTimeout(() => {
+            toasts.forEach(toast => {
+                toast.style.display = 'none';
+            });
+        }, 500);
+    }, 3000);
+    
+    return (
+            <div className={`toast-popup ${type}`}>
+                {icon}
+                <div>
+                    <h4 className='title'>{title}</h4>
+                    <p>{message}</p>
+                </div>
+            </div>
+    )
+}
